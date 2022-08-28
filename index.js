f76d7aa6653c879e97fb681ced699c75d9051a8c
('Jsl') <100409086+Afx-Abu@users.noreply.github.com>
2022-08-28 14:38:30 +0530

    `FIX 🤤❤️`

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const client = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const start = async () => {
 	try {
-		console.log(`levanter ${VERSION}`)
+		console.log(METRO-BOT ${VERSION}`)
 		await DATABASE.sync()
-		console.log('DB syncing')
+		console.log('DATABSE CONNECTED ✅')
 		await client.connect()
 	} catch (error) {
 		console.error(error)
