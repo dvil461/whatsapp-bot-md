08dcf554c5166d8132f30480801d4ece2b601af5
Vɪᴢ <80111660+Viz-Zer@users.noreply.github.com>
2022-09-09 10:06:18 +0530

    😹🎈

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const client = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const start = async () => {
 	try {
-		console.log(METRO-BOT ${VERSION}`)
+		console.log(`METRO-BOT ${VERSION}`)
 		await DATABASE.sync()
 		console.log('DATABSE CONNECTED ✅')
 		await client.connect()
