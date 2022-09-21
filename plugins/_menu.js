06f524fec45efa5261c5d606596a0e80022a9f66
Vɪᴢ <80111660+Viz-Zer@users.noreply.github.com>
2022-09-21 14:12:30 +0530

    💓

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -20,45 +20,18 @@ bot.addCommand(
 		dontAddCommandList: true,
 	},
 	async (message, match) => {
-		const date = new Date()
-		let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
-╰────────────────╯
-
-╭────────────────
-│ Prefix : ${PREFIX}
-│ User : ${message.pushName}
-│ Time : ${date.toLocaleTimeString()}
-│ Day : ${date.toLocaleString('en', { weekday: 'long' })}
-│ Date : ${date.toLocaleDateString('hi')}
-│ Version : ${VERSION}
-│ Plugins : ${PLUGINS.count}
-│ Ram : ${getRam()}
-│ Uptime : ${getUptime('t')}
-╰────────────────
-╭────────────────
-`
-		const commands = []
+		let msg = ''
 		bot.commands.map(async (command, index) => {
 			if (
 				command.dontAddCommandList === false &&
 				command.pattern !== undefined
 			) {
-				commands.push(ctt(command.pattern))
+				msg += `${index} ${ctt(command.pattern)}\n${command.desc}\n\n`
 			}
 		})
-		commands.forEach((command, i) => {
-			CMD_HELP += `│ ${i + 1} ${addSpace(
-				i + 1,
-				commands.length
-			)}${textToStylist(command.toUpperCase(), 'mono')}\n`
-		})
-		CMD_HELP += `╰────────────────`
-		return await message.sendMessage('' + CMD_HELP + '')
+		await message.send('```' + msg.trim() + '```')
 	}
 )
-
-
 bot.addCommand(
 	{
 		pattern: 'help ?(.*)',
